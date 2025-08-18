8212a84af49cd25d5726e2e89539bcca8b25defb
Vadim Elshin <elshinvadim@gmail.com>
2025-08-18 19:54:17 +0200

    LoginGUI Functionality

---
+++ BankingApp/src/db_objs/MyJDBC.java
@@ -0,0 +1,34 @@
+package db_objs;
+
+import java.math.BigDecimal;
+import java.sql.*;
+
+public class MyJDBC {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/bankapp?useSSL=false";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "Live2015";
+
+    public static User validateLogin(String username, String password){
+        try{
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM users WHERE username = ? AND password = ?"
+            );
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()){
+                int userId = resultSet.getInt("id");
+
+                BigDecimal currentBalance = resultSet.getBigDecimal("current_balance");
+
+                return new User(userId, username, password, currentBalance);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
